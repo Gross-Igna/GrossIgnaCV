c24ffaff0a79089ea7c49433a49e07cf7160a800
unknown <grossignacio01@gmail.com>
2021-12-29 17:12:06 -0300

    mobileAlert test 1

--- src/components/MobileAlert.js
+++ src/components/MobileAlert.js
@@ -4,31 +4,29 @@ import sad_phone from '../img/sad_phone.png'
 
 export default function MobileAlert() {
 
-    /*const [mobileAlert, setMobileAlert] = useState(false);*/
     let mobileAlert = false;
-
-    function detectMob() {
-        return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
-      }
-
-    if (!detectMob){
+    if (window.screen.width < 800 || window.screen.height < 600){
         mobileAlert = true;
     }
 
-    return (
-        <div className='mobileAlert' style={{zIndex: mobileAlert? '15' : '-5', backgroundColor: mobileAlert? '#1e023df5' : 'transparent'}}>
-            <div className='alertContent'>
-                <div className='alertTitle'>
-                    ¡Lo siento!
+    if(mobileAlert){
+        return (
+            <div className='mobileAlert' style={{zIndex: mobileAlert? '15' : '-5', backgroundColor: mobileAlert? '#1e023df5' : 'transparent'}}>
+                <div className='alertContent'>
+                    <div className='alertTitle'>
+                        ¡Lo siento!
+                    </div>
+                    <img className='alertImage'src={sad_phone} alt='sad phone'></img>
+                    <div className='alertText'>
+                        Este sitio web ha sido diseñado para ser ejecutado en pantallas de computadora.<br></br>
+                        Vas a ser redirigido <a href='http://grossignacio.ga/'>grossignacio.ga</a> para que puedas
+                        visualizar mi currículum en tu teléfono.
+                    </div>
+                    <btn className='alertBtn' onClick={ () => window.location.href = 'http://grossignacio.ga/'}><b>Aceptar</b></btn>
                 </div>
-                <img className='alertImage'src={sad_phone} alt='sad phone'></img>
-                <div className='alertText'>
-                    Este sitio web ha sido diseñado para ser ejecutado en pantallas de computadora.<br></br>
-                    Vas a ser redirigido <a href='http://grossignacio.ga/'>grossignacio.ga</a> para que puedas
-                    visualizar mi currículum en tu teléfono.
-                </div>
-                <btn className='alertBtn'>Aceptar</btn>
             </div>
-        </div>
-    )
+        )
+    }else{
+        return( <div></div> )
+    }
 }
